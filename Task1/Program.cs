d2b03a18224ae22b01d400743d945dc23e7a4017
Stitch710 <dima_pichugin_2016@mail.ru>
2022-03-07 13:11:13 +0300

    Добавьте файлы проекта.

---
+++ Task1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("введите длину сторон");
+            if (int.TryParse(Console.ReadLine(), out int a)
+                && int.TryParse(Console.ReadLine(), out int b)
+                && a > 0
+                && b > 0)
+                Console.WriteLine($"Площадь: {a * b}");
+            else
+                Console.WriteLine("введите корректные значения");
+        }
+    }
+}
